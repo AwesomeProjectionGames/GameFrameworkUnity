2b2961cddf4fb41ffc7f51f39cabf6c6ef124474
eliott <eliott.jaquier@gmail.com>
2025-12-20 00:29:59 +0100

    Force to have a camera in ISpectate implementations

--- POV/GameObjectUnityEventsSpectated.cs
+++ POV/GameObjectUnityEventsSpectated.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 
+using AwesomeProjectionCoreUtils.Extensions;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,16 +10,21 @@ namespace GameFramework.POV
     /// A simple MonoBehaviour that invokes UnityEvents when spectating starts and ends.
     /// Intended to enable things like UI, camera, etc.
     /// </summary>
+    [RequireComponent(typeof(ICamera))]
     public class GameObjectUnityEventsSpectated : MonoBehaviour, ISpectate
     {
         public bool IsSpectating { get; set; }
         public ISpectateController? SpectateController { get; set; }
+        public ICamera GetSpectateCamera() => _camera;
         
         [SerializeField] private UnityEvent? onSpectate;
         [SerializeField] private UnityEvent? onSpectateEnd;
+
+        private ICamera _camera = null!;
         
         private void Awake()
         {
+            _camera = GetComponent<ICamera>();
             OnStopSpectating();
         }
         
